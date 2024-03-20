a6213daecf9a74a841e49448aea206de83d16920
Demian Calleros <demiancalleros0@gmail.com>
2024-03-19 19:31:51 -0600

    mainStructure part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBarUACJ from './componentes/NavBar';
+import EventoUACJ from './componentes/EventoContainer';
+
+import {Spacer} from "@nextui-org/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarUACJ></NavBarUACJ>
+      <h1>¡Proximo eventos!</h1>
+      <div className="flex center">
+        <EventoUACJ />
+        <Spacer x={4} />
+        <EventoUACJ />
+        <Spacer x={4} />
+        <EventoUACJ />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <main className="light text-foreground bg-background">
+        <App />
+      </main>
+    </NextUIProvider>
   </React.StrictMode>
 );
 
