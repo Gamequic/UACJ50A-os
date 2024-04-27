a171623df5232f3756cd799170cf4511135ff902
Gamequic <demiancalleros0@gmail.com>
2024-04-26 19:02:36 -0600

    Upload files

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
             preview='Viernes 12, 19 y 26 de abril y 3 de mayo'
             info='El Instituto de Ingeniería y Tecnología de la UACJ invita a la comunidad universitaria a participar en un torneo de Ping Pong en conmemoración de 50 años de ingeniería en la institución. El evento se llevará a cabo los días viernes 12, 19 y 26 de abril y 3 de mayo en la Plaza de la Fraternidad del instituto.'
             qr={`${process.env.PUBLIC_URL}/Photos/QR's/PingPong.svg`}
+            link={`${process.env.PUBLIC_URL}/Files/Convocatoria torneo de ping pong.docx`}
           />
           <EventoUACJ
             image={`${process.env.PUBLIC_URL}/Photos/Futbol.png`}
@@ -27,6 +28,7 @@ function App() {
             preview='Viernes 12, 19 y 26 de abril y 3 de mayo.'
             info='Torneo recreativo de Fútbol calle mixto en conmemoración de sus 50 años de ingeniería. El evento se llevará a cabo los días viernes 12, 19 y 26 de abril y 3 de mayo en la Plaza de la Fraternidad del instituto. Se invita a formar equipos mixtos de hasta 8 integrantes, con dos jugadoras y dos jugadores en el cuadro en juego.'
             qr={`${process.env.PUBLIC_URL}/Photos/QR's/Futbol.svg`}
+            link={`${process.env.PUBLIC_URL}/Files/Convocatoria torneo mixto de futbol IIT.docx`}
           />
           <EventoUACJ
             image={`${process.env.PUBLIC_URL}/Photos/Arduino.jpg`}
@@ -34,12 +36,14 @@ function App() {
             preview='06 de mayo de 2024, de 10:00 AM a 02:00 PM en el vestíbulo Y-4.'
             info='Competencia de Mouseke-olimpiadas como parte de las celebraciones del 30 Aniversario del Instituto, el 06 de mayo de 2024, de 10:00 AM a 02:00 PM en el vestíbulo Y-4. Se considerarán tres competidores por equipo, inscritos en el semestre actual de licenciatura, con un representante (profesor) por equipo.'
             qr={`${process.env.PUBLIC_URL}/Photos/QR's/Mousek.svg`}
+            link={`${process.env.PUBLIC_URL}/Files/Mousek.docx`}
           />
           <EventoUACJ
             image={`${process.env.PUBLIC_URL}/Photos/Programacion.jpg`}
             title='Maratón de Programación y Algoritmia'
             preview='El maratón se divide en dos categorías: Algoritmia, el 9 de abril de 2024, y Programación, el 9 y 10 de abril.'
             info='El maratón será dividido en dos categorías, Algoritmia se llevará a cabo el 9 de abril de 2024, de 10:00 de la mañana a 6:00 de la tarde, mientras que el de Programación se celebrará el 9 y 10 de abril del presente año, en el Instituto de Ingeniería y Tecnología (IIT) y en Ciudad Universitaria, los eventos serán simultáneos en los institutos y contarán con un receso. '
+            link={`${process.env.PUBLIC_URL}/Files/Reglamento Maraton de programacion y algoritmia.docx`}
           />
         </div>
         <Spacer y={6} />
