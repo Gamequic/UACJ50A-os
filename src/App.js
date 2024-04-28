12c081d574ef16fcd361b358b63d990da7521f7c
Gamequic <demiancalleros0@gmail.com>
2024-04-27 19:42:58 -0600

    Catapulta, fix qr links

--- src/App.js
+++ src/App.js
@@ -25,14 +25,14 @@ function App() {
                 <EventUACJ
                   link={`${process.env.PUBLIC_URL}/Files/Convocatoria torneo de ping pong.docx`}
                   info={'El Instituto de Ingeniería y Tecnología de la UACJ invita a la comunidad universitaria a participar en un torneo de Ping Pong en conmemoración de 50 años de ingeniería en la institución. El evento se llevará a cabo los días viernes 12, 19 y 26 de abril y 3 de mayo en la Plaza de la Fraternidad del instituto.'}
-                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/PingPong.svg`}
+                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/PingPong.png`}
                 />
               </AccordionItem>
               <AccordionItem key="2" aria-label="Fútbol" title="Fútbol">
               <EventUACJ
                   link={`${process.env.PUBLIC_URL}/Files/Convocatoria torneo mixto de futbol IIT.docx`}
                   info={'Torneo recreativo de Fútbol calle mixto en conmemoración de sus 50 años de ingeniería. El evento se llevará a cabo los días viernes 12, 19 y 26 de abril y 3 de mayo en la Plaza de la Fraternidad del instituto. Se invita a formar equipos mixtos de hasta 8 integrantes, con dos jugadoras y dos jugadores en el cuadro en juego.'}
-                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/Futbol.svg`}
+                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/FutBol.png`}
                 />
               </AccordionItem>
             </Accordion>
@@ -50,13 +50,20 @@ function App() {
                 <EventUACJ
                   link={`${process.env.PUBLIC_URL}/Files/Mousek.docx`}
                   info={'Competencia de Mouseke-olimpiadas como parte de las celebraciones del 30 Aniversario del Instituto, el 06 de mayo de 2024, de 10:00 AM a 02:00 PM en el vestíbulo Y-4. Se considerarán tres competidores por equipo, inscritos en el semestre actual de licenciatura, con un representante (profesor) por equipo.'}
-                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/Mousek.svg`}
+                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/MouseK.png`}
                 />
               </AccordionItem>
               <AccordionItem key="2" aria-label="Maratón de Programación y Algoritmia" title="Maratón de Programación y Algoritmia">
-              <EventUACJ
-                  link={`${process.env.PUBLIC_URL}/Files/Reglamento Maraton de programacion y algoritmia.docx`}
-                  info={'El maratón será dividido en dos categorías, Algoritmia se llevará a cabo el 9 de abril de 2024, de 10:00 de la mañana a 6:00 de la tarde, mientras que el de Programación se celebrará el 9 y 10 de abril del presente año, en el Instituto de Ingeniería y Tecnología (IIT) y en Ciudad Universitaria, los eventos serán simultáneos en los institutos y contarán con un receso.'}
+                <EventUACJ
+                    link={`${process.env.PUBLIC_URL}/Files/Reglamento Maraton de programacion y algoritmia.docx`}
+                    info={'El maratón será dividido en dos categorías, Algoritmia se llevará a cabo el 9 de abril de 2024, de 10:00 de la mañana a 6:00 de la tarde, mientras que el de Programación se celebrará el 9 y 10 de abril del presente año, en el Instituto de Ingeniería y Tecnología (IIT) y en Ciudad Universitaria, los eventos serán simultáneos en los institutos y contarán con un receso.'}
+                  />
+              </AccordionItem>
+              <AccordionItem key='3' aria-label='Concurso Catapulta' title='Concurso Catapulta'>
+                <EventUACJ
+                  link={`${process.env.PUBLIC_URL}/Files/convocatoria concurso catapultas  ene_jun 2024_1.pdf`}
+                  info={'El Instituto de Ingeniería y Tecnología, junto con la Rama estudiantil ASQ-UACJ, invita a estudiantes universitarios a participar en el concurso La Catapulta, que busca fomentar el aprendizaje en estadística industrial y calidad. Los equipos de 4 a 6 integrantes analizarán y optimizarán el rendimiento de una catapulta en dos fases. El evento se llevará a cabo el 6 de mayo de 2024, otorgando premios en efectivo a los mejores proyectos.'}
+                  qr={`${process.env.PUBLIC_URL}/Photos/Logos/QR's/Catapulta.png`}
                 />
               </AccordionItem>
             </Accordion>
